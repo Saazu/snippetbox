1c7f956ec81eab9e54e1003090b47f410e27c5e7
Alfred Delle <alfred.delle@yale.edu>
2019-08-06 16:38:20 +0000

    Added gitignore file

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module saazu.net/snippetbox
 
 go 1.12
 
-require github.com/go-sql-driver/mysql v1.4.1 // indirect
+require (
+	github.com/go-sql-driver/mysql v1.4.1 // indirect
+	github.com/gorilla/rpc v1.2.0 // indirect
+)
--- usersetup.sql
+++ usersetup.sql
@@ -1,4 +1,4 @@
 CREATE USER 'web'@'localhost';
 GRANT SELECT, INSERT ON snippetbox.* TO 'web'@'localhost';
 -- Important: Make sure to swap 'pass' with a password of your own choosing.
-ALTER USER 'web'@'localhost' IDENTIFIED BY 'password';
+ALTER USER 'web'@'localhost' IDENTIFIED BY '**password***';
