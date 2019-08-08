86b178b166c7e90fb763d6cd94cf6ad1bf737886
Alfred Tuotol Delle <alfred.delle@yale.edu>
2019-08-08 16:50:54 +0000

    Update usersetup.sql

--- usersetup.sql
+++ usersetup.sql
@@ -1,4 +1,5 @@
 CREATE USER 'web'@'localhost';
 GRANT SELECT, INSERT ON snippetbox.* TO 'web'@'localhost';
--- Important: Make sure to swap 'pass' with a password of your own choosing.
+--swap 'pass' with a password of your own choosing.
+--I use my default user rather than web because setting up a new user for mysql is too much of a pain right now
 ALTER USER 'web'@'localhost' IDENTIFIED BY '**password***';
